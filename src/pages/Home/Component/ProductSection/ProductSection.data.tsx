3402a5715d4216b131845864d704cbb26e9b7e50
Phan Long <longbp166@gmail.com>
2024-09-11 15:39:24 +0700

    refactor: 💡 add product sample data

--- src/pages/Home/Component/ProductSection/ProductSection.data.tsx
+++ src/pages/Home/Component/ProductSection/ProductSection.data.tsx
@@ -1,5 +1,6 @@
 import Title from "@/components/Title";
 import PreProduct1 from '@/assets/example/product/ly_holo_tokinosora_7th_banner_en_2_1725513666_480x.webp';
+import Product2 from '@/assets/example/product/HL_ASMR_8m_AZKi_cafe_JP_SQ_480x.webp';
 
 export const ProductData = [
     {
@@ -32,5 +33,37 @@ export const ProductData = [
                 price: 1100,
             },
         ]
-    }
+    },
+    {
+        id: 2,
+        key: "Available Product",
+        title: <Title title="Hàng có sẵn"/>,
+        product: [
+            {
+                id: "pre-1",
+                name: "Tokino Sora 7th Anniversary Celebration",
+                img: Product2,
+                price: 1100,
+            },
+            {
+                id: "pre-2",
+                name: "Tokino Sora 7th Anniversary Celebration",
+                img: Product2,
+                price: 1100,
+            },
+            {
+                id: "pre-3",
+                name: "Tokino Sora 7th Anniversary Celebration",
+                img: Product2,
+                price: 1100,
+            },
+            {
+                id: "pre-4",
+                name: "Tokino Sora 7th Anniversary Celebration",
+                img: Product2,
+                price: 1100,
+            },
+        ]
+    },
+    
 ]
