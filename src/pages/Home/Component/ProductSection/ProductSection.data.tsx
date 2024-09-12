f30df3b1c049f347c108e1780e29e81738ba7b96
Phan Long <longbp166@gmail.com>
2024-09-12 08:20:59 +0700

    refactor: 💡 add sample data

--- src/pages/Home/Component/ProductSection/ProductSection.data.tsx
+++ src/pages/Home/Component/ProductSection/ProductSection.data.tsx
@@ -1,6 +1,7 @@
 import Title from "@/components/Title";
 import PreProduct1 from '@/assets/example/product/ly_holo_tokinosora_7th_banner_en_2_1725513666_480x.webp';
 import Product2 from '@/assets/example/product/HL_ASMR_8m_AZKi_cafe_JP_SQ_480x.webp';
+import Product3 from '@/assets/example/product/holomini_Myth_bannar_JP_1_480x.webp';
 
 export const ProductData = [
     {
@@ -40,30 +41,60 @@ export const ProductData = [
         title: <Title title="Hàng có sẵn"/>,
         product: [
             {
-                id: "pre-1",
+                id: "able-1",
+                name: "Tokino Sora 7th Anniversary Celebration",
+                img: Product3,
+                price: 1100,
+            },
+            {
+                id: "able-2",
+                name: "Tokino Sora 7th Anniversary Celebration",
+                img: Product3,
+                price: 1100,
+            },
+            {
+                id: "able-3",
+                name: "Tokino Sora 7th Anniversary Celebration",
+                img: Product3,
+                price: 1100,
+            },
+            {
+                id: "able-4",
+                name: "Tokino Sora 7th Anniversary Celebration",
+                img: Product3,
+                price: 1100,
+            },
+        ]
+    },
+    {
+        id: 3,
+        key: "Ending soon",
+        title: <Title title="Sắp kết thúc"/>,
+        product: [
+            {
+                id: "end-1",
                 name: "Tokino Sora 7th Anniversary Celebration",
                 img: Product2,
                 price: 1100,
             },
             {
-                id: "pre-2",
+                id: "end-2",
                 name: "Tokino Sora 7th Anniversary Celebration",
                 img: Product2,
                 price: 1100,
             },
             {
-                id: "pre-3",
+                id: "end-3",
                 name: "Tokino Sora 7th Anniversary Celebration",
                 img: Product2,
                 price: 1100,
             },
             {
-                id: "pre-4",
+                id: "end-4",
                 name: "Tokino Sora 7th Anniversary Celebration",
                 img: Product2,
                 price: 1100,
             },
         ]
     },
-    
 ]
