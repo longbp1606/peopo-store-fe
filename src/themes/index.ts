1905c6238bf410e2721949d41868796d74812db1
Phan Long <longbp166@gmail.com>
2024-09-14 17:44:55 +0700

    style: 💄 add light primary color

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -6,6 +6,7 @@ export const theme = {
     dark: '',
     colors: {
         primary: PRIMARY_COLOR,
+        lightPrimary: 'rgba(69,218,241,0.1)',
         blueGradient: 'linear-gradient(45deg, rgba(5,235,254,1) 0%, rgba(232,3,226,1) 100%)',
         greenGradient: 'linear-gradient(45deg, rgba(250,229,71,1) 0%, rgba(7,196,201,1) 100%)',
         pinkGradient: 'linear-gradient(45deg, rgba(255,7,109,1) 0%, rgba(254,173,231,1) 100%)',
