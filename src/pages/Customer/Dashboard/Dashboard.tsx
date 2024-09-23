6a9db4c12dbd92ae6d65f4d9d4f60b69e9622b5a
Phan Long <longbp166@gmail.com>
2024-09-23 16:45:18 +0700

    feat: 🎸 add dashboard section

---
+++ src/pages/Customer/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import * as Styled from './Dashboard.styled';
+
+const Dashboard = () => {
+    return (
+        <>
+            <Styled.DashboardWrapper>
+
+            </Styled.DashboardWrapper>
+        </>
+    )
+}
+
+export default Dashboard
