892b10350d94018c87338b92f3bf1ecd1f22a480
Phan Long <longbp166@gmail.com>
2024-09-23 16:46:48 +0700

    refactor: 💡 add routes for customer page

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,29 +1,44 @@
 import config from "@/config";
 import MainLayout from "@/layouts/MainLayout"
 import NotFound from "@/pages/404/404";
+import CustomerLayout from "@/pages/Customer/CustomerLayout";
+import Dashboard from "@/pages/Customer/Dashboard";
+import Profile from "@/pages/Customer/Profile";
 import Home from "@/pages/Home/Home";
 import ProductDetail from "@/pages/ProductDetail/ProductDetail";
 import Products from "@/pages/Products";
 
 
 const MainRouter = () => {
-    return <MainLayout/>
+    return <MainLayout />
 };
+
+const CustomerRouter = () => {
+    return <CustomerLayout />;
+}
 
 const publicRoutes = {
     children: [
-        { path: config.routes.public.home, element: <Home/> },
-        { path: config.routes.public.product, element: <Products/>},
-        { path: config.routes.public.productDetail, element: <ProductDetail/>}
+        { path: config.routes.public.home, element: <Home /> },
+        { path: config.routes.public.product, element: <Products /> },
+        { path: config.routes.public.productDetail, element: <ProductDetail /> }
     ]
 };
 
-const notFoundRoutes = { path: '*', element: <NotFound/>}
+const customerRoutes = {
+    element: <CustomerRouter />,
+    children: [
+        { path: config.routes.customer.dashboard, element: <Dashboard /> },
+        { path: config.routes.customer.profile, element: <Profile /> },
+    ]
+}
+
+const notFoundRoutes = { path: '*', element: <NotFound /> }
 
 const MainRoutes = {
     path: '/',
-    element: <MainRouter/>,
-    children: [publicRoutes, notFoundRoutes],
+    element: <MainRouter />,
+    children: [publicRoutes, customerRoutes, notFoundRoutes],
 };
 
 export default MainRoutes;
