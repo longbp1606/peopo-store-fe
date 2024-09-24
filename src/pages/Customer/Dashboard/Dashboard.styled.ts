627c28294b870e9d838e1ccf9758c39948ed7c19
Phan Long <longbp166@gmail.com>
2024-09-24 16:48:50 +0700

    feat: 🎸 complete dashboard section

--- src/pages/Customer/Dashboard/Dashboard.styled.ts
+++ src/pages/Customer/Dashboard/Dashboard.styled.ts
@@ -1,3 +1,63 @@
+import { theme } from "@/themes";
+import { Flex, Image } from "antd";
 import styled from "styled-components";
 
-export const DashboardWrapper = styled.div``;
+export const DashboardWrapper = styled.div`
+    width: 100%;
+    padding: 24px 28px;
+    border-radius: 15px;
+    border: 1px solid ${theme.colors.borderDefault};
+    
+    & h2.ant-typography {
+        color: ${theme.colors.primary};
+        width: 100%;
+    }
+
+    & h3.ant-typography {
+        color: ${theme.colors.primary};
+    }
+`;
+
+export const ProfileSection = styled.div`
+    width: 100%;
+    background-color: ${theme.colors.lightPrimary};
+    padding: 24px 28px;
+    border-radius: 20px;
+`;
+
+export const ProfileSummary = styled(Flex)`
+
+`;
+
+export const AvatarHolder = styled.div`
+    overflow: hidden;
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+    border: 2px solid ${theme.colors.primary}
+`;
+
+export const AvatarImage = styled(Image)`
+    width: 100%;
+`;
+
+export const ProfileDetail = styled.div`
+    & span.ant-typography {
+        color: ${theme.colors.textPrimary}
+    }
+`;
+
+export const OrderSection = styled.div``;
+
+export const AddressSection = styled.div`
+    & .ant-row {
+        padding: 24px 28px;
+        margin-bottom: 2em;
+        border-radius: 20px;
+        box-shadow: 2px 2px 10px ${theme.colors.shadowForm};
+    }
+
+    & .ant-btn {
+        padding: 20px;
+    }
+`;
--- src/pages/Customer/Dashboard/Dashboard.tsx
+++ src/pages/Customer/Dashboard/Dashboard.tsx
@@ -1,10 +1,114 @@
+import { Button, Col, Divider, Flex, Row, Space, Table, Typography } from 'antd';
 import * as Styled from './Dashboard.styled';
+import { AddressSampleData, DashboardData, DataType, OrderData, OrderTableSource, ProfileSampleData } from '../Customer.data';
+import { Link } from 'react-router-dom';
+import config from '@/config';
+import { FaLocationDot } from 'react-icons/fa6';
+import { theme } from '@/themes';
+
+const { Title, Text } = Typography;
 
 const Dashboard = () => {
     return (
         <>
             <Styled.DashboardWrapper>
+                <Space direction='vertical' style={{ width: '100%' }}>
+                    <Title level={2}>Bảng điều khiển của khách hàng</Title>
 
+                    <Styled.ProfileSection>
+                        <Styled.ProfileSummary gap={10}>
+                            <Styled.AvatarHolder>
+                                <Styled.AvatarImage
+                                    src={ProfileSampleData.avatar}
+                                    alt={ProfileSampleData.fullname}
+                                    preview={false}
+
+                                />
+                            </Styled.AvatarHolder>
+
+                            <Space direction='vertical' size='middle'>
+                                {DashboardData[0].component.map((item) => (
+                                    <Text key={item.key}>
+                                        {item.label}
+                                        <Text strong> {item.value}</Text>
+                                    </Text>
+                                ))}
+                            </Space>
+                        </Styled.ProfileSummary>
+                        <Divider />
+                        <Styled.ProfileDetail>
+                            <Title level={3}>Thông tin tài khoản</Title>
+
+                            {DashboardData[1].component.map((item) => (
+                                <Row key={item.key}>
+                                    <Col lg={12}>
+                                        <Text strong>{item.label}</Text>
+                                    </Col>
+
+                                    <Col>
+                                        <Text>{item.value} </Text>
+                                    </Col>
+                                </Row>
+                            ))}
+                        </Styled.ProfileDetail>
+                    </Styled.ProfileSection>
+
+                    <Divider />
+
+                    <Styled.OrderSection>
+                        <Flex justify='space-between'>
+                            <Title level={3}>NHỮNG ĐƠN HÀNG GẦN ĐÂY</Title>
+                            <Link to={config.routes.customer.order}>Xem tất cả</Link>
+                        </Flex>
+
+                        <Table<DataType>
+                            columns={OrderTableSource}
+                            dataSource={OrderData}
+                        />
+                    </Styled.OrderSection>
+
+                    <Divider />
+
+                    <Styled.AddressSection>
+                        <Title level={3}>ĐỊA CHỈ GIAO HÀNG</Title>
+
+                        {AddressSampleData.map((info) => (
+                            <Row 
+                                key={info.key} 
+                                justify={'space-between'}
+                                align={'middle'}
+                            >
+                                <Col lg={8}>
+                                    <Space direction='vertical'>
+                                        {info.name}
+                                        {info.address}
+                                        {info.phoneNumber}
+                                    </Space>
+                                </Col>
+
+                                <Col lg={6}>
+                                    {info.default &&
+                                        <Text style={{color: `${theme.colors.primary}`}}>
+                                            <FaLocationDot/> Địa chỉ mặc định
+                                        </Text>
+                                    }
+                                </Col>
+
+                                <Col lg={6}>
+                                    <Flex 
+                                        vertical
+                                        gap={20}
+                                    >
+                                        <Button type='primary' block>Chỉnh sửa</Button>
+                                        {info.default &&
+                                            <Button>Đặt làm mặc định</Button>}
+                                        <Button danger block>Xóa</Button>
+                                    </Flex>
+                                </Col>
+                            </Row>
+                        ))}
+                    </Styled.AddressSection>
+                </Space>
             </Styled.DashboardWrapper>
         </>
     )
