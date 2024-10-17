eca9f699c08170c3b1977ab0db1968bc91a8998a
Phan Long <longbp166@gmail.com>
2024-10-17 15:30:32 +0700

    feat: 🎸 add error color

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -7,10 +7,12 @@ export const theme = {
     colors: {
         primary: PRIMARY_COLOR,
         lightPrimary: 'rgba(69,218,241,0.1)',
+        error: '#FF0000',
         blueGradient: 'linear-gradient(45deg, rgba(5,235,254,1) 0%, rgba(232,3,226,1) 100%)',
         greenGradient: 'linear-gradient(45deg, rgba(250,229,71,1) 0%, rgba(7,196,201,1) 100%)',
         pinkGradient: 'linear-gradient(45deg, rgba(255,7,109,1) 0%, rgba(254,173,231,1) 100%)',
         orangeGradient: 'linear-gradient(45deg, rgba(253,7,85,1) 0%, rgba(253,208,1,1) 100%)',
+        border: '#BFBFBF',
         textPrimary: 'rgba(0, 0, 0, 0.85)',
         textSecondary: 'rgba(0, 0, 0, 0.45)',
         divider: 'rgba(5, 5, 5, 0.06)',
