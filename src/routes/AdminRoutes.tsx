83088fa02f2438deccad470591c33b0fd98964bc
Phan Long <longbp166@gmail.com>
2024-11-19 16:40:09 +0700

    feat: 🎸 add manage order route

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -1,6 +1,7 @@
 import config from "@/config";
 import AdminLayout from "@/layouts/AdminLayout"
 import Dashboard from "@/pages/Admin/Dashboard";
+import ManageOrder from "@/pages/Admin/ManageOrder";
 
 
 const AdminRouter = () => {
@@ -11,7 +12,8 @@ const AdminRoutes = {
     path: config.routes.admin.dashboard,
     element: <AdminRouter />,
     children: [
-        { path: config.routes.admin.dashboard, element: <Dashboard /> }
+        { path: config.routes.admin.dashboard, element: <Dashboard /> },
+        { path: config.routes.admin.manageOrder, element: <ManageOrder />},
     ]
 };
 
